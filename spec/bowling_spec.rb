0b4bd968a9ca746c4277ce702654e00e59241fec
Keiichiro Yamamoto <k.yanmar@gmail.com>
2014-07-03 14:42:36 +0000

    remove simple Rspec sample code (bowling*rb)

--- spec/bowling_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'bowling'
-
-describe Bowling, "#score" do
-  it "returns 0 for all gutter game" do
-    bowling = Bowling.new
-    20.times { bowling.hit(0) }
-    expect(bowling.score).to eq(0)
-  end
-end
